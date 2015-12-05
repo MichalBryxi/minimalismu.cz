de8d22d03ee8c120ecb41f5d4a7e41a8dacc26de
Michal Bryxí <michal.bryxi@gmail.com>
2015-12-06 00:08:13 +0100

    Cleanup from console.log

--- frontend/vendor/agency.js
+++ frontend/vendor/agency.js
@@ -7,7 +7,6 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(window).load(function () {
     $('a.page-scroll').bind('click', function(event) {
-      console.log('foo');
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
